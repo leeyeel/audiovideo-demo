1097e9fe80fe6d2158e388a5f726e55bbd93d015
leo <mumuli52@gmail.com>
2020-09-15 00:45:08 +0800

    增加倍速播放及wav文件信息，未完成

--- pcm.py
+++ pcm.py
@@ -45,16 +45,37 @@ class pcm(object):
                     outf.write(strs)
         print('function: %s done' % sys._getframe().f_code.co_name)
 
-    def soft_speed(self, speed = 1.0):
-        print('function: %s is empty' % sys._getframe().f_code.co_name)
+    def soft_speed(self, speed = 2.0):
+        '非常简单的加速两倍播放,抽帧实现'
+        with open(self.iname, 'rb') as inf:
+            offsets = inf.read(self.offset)
+            with open(self.oname, 'wb') as outf:
+                while True:
+                    left = inf.read(self.format)
+                    if not left: break
+                    outf.write(left)
+                    right = inf.read(self.format)
+                    if not right: break
+        print('function: %s is done' % sys._getframe().f_code.co_name)
         
     def samplerate(self, rate = 48000):
         print('function: %s is empty' % sys._getframe().f_code.co_name)
 
-    def wav_info(self):
+    def wav_info(self, paramlist = []):
         with open(self.iname, 'rb') as inf:
-            print('function: %s is empty' % sys._getframe().f_code.co_name)
+            header = struct.unpack('<4cI4c4cihhiihh4ci', inf.read(44))
+            print(header)
+            headernamelist = ['ChunkID', 'ChunkSize', 'Format', 'SubChun1kID', 'SubChunk1Size', 'AudioFormat', 'NumChannels',
+                                'SampleRate', 'ByteRate', 'BlockAlign', 'BitsPerSample', 'SubChunk2ID', 'SubChunk2Size']
+            headervaluelist = [header[0:4], header[4], header[5:9], header[9:13], header[13], header[14], 
+                    header[15], header[16], header[17], header[18], header[19], header[20], header[21]]
+            for (name, value) in zip(headernamelist, headervaluelist):
+                print(name + ':' + str(value))
+
+            print('function: %s is done' % sys._getframe().f_code.co_name)
 
 if __name__=="__main__":
     pcm = pcm('13_hush_hush.wav', 44)
-    pcm.soft_volume(3)
+    #pcm.soft_volume(3)
+    #pcm.soft_speed()
+    pcm.wav_info()
