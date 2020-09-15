80df8d642c1847e3fd7efb2198dad95d45d8c404
leo <mumuli52@gmail.com>
2020-09-16 01:00:14 +0800

    modified pcm.wav_info

--- pcm.py
+++ pcm.py
@@ -18,6 +18,7 @@ class pcm(object):
             self.rname = namelist[0] + '-right'
         self.format = 2
         self.channels = channels
+        self.format_tag = {1:'pcm', 3:'FLOAT', 6:'G711-a', 7:'G711-u', 0xFFFE:'EXTENDED'}
 
     def split_left_right(self):
         with open(self.iname, 'rb') as inf:
@@ -57,22 +58,44 @@ class pcm(object):
                     right = inf.read(self.format)
                     if not right: break
         print('function: %s is done' % sys._getframe().f_code.co_name)
+
+    def __find_format(self, inf):
+        ID = struct.unpack('>4c', inf.read(4))
+        if ID == (b'f', b'm', b't', b' '):
+            return 0
+        else:
+            size = struct.unpack('<I', inf.read(4))
+            inf.read(size[0])
+            self.__find_format(inf)
         
     def samplerate(self, rate = 48000):
         print('function: %s is empty' % sys._getframe().f_code.co_name)
 
     def wav_info(self, paramlist = []):
         with open(self.iname, 'rb') as inf:
-            header = struct.unpack('<4cI4c4cihhiihh4ci', inf.read(44))
-            print(header)
-            headernamelist = ['ChunkID', 'ChunkSize', 'Format', 'SubChun1kID', 'SubChunk1Size', 'AudioFormat', 'NumChannels',
-                                'SampleRate', 'ByteRate', 'BlockAlign', 'BitsPerSample', 'SubChunk2ID', 'SubChunk2Size']
-            headervaluelist = [header[0:4], header[4], header[5:9], header[9:13], header[13], header[14], 
-                    header[15], header[16], header[17], header[18], header[19], header[20], header[21]]
-            for (name, value) in zip(headernamelist, headervaluelist):
-                print(name + ':' + str(value))
-
-            print('function: %s is done' % sys._getframe().f_code.co_name)
+            riffID = struct.unpack('>4c', inf.read(4))
+            riff_size = struct.unpack('<I', inf.read(4))
+            riff_type = struct.unpack('>4c', inf.read(4))
+            if riff_type == (b'W', b'A', b'V', b'E'):
+                self.__find_format(inf)
+                format_size = struct.unpack('<I', inf.read(4))
+                format_tag = struct.unpack('<H', inf.read(2))
+                format_channels = struct.unpack('<H', inf.read(2))
+                format_samplerate = struct.unpack('<I', inf.read(4))
+                format_byterate = struct.unpack('<I', inf.read(4))
+                format_align = struct.unpack('<H', inf.read(2))
+                format_format = struct.unpack('<H', inf.read(2))
+                if format_tag == 0xFFFE:
+                    format_cbsize = struct.unpack('<H', inf.read(2))
+                    format_cb = struct.unpack('<B', inf.read(22))
+                print('format tag is: ' + str(self.format_tag[format_tag[0]]))
+                print('channels : ' + str(format_channels[0]))
+                print('format : ' + str(format_format[0]))
+                print('samplerate is: ' + str(format_samplerate[0]))
+            elif riff_type == ('L', 'I', 'S', 'T'):
+                print('list')
+            else:
+                print('unrecognized format')
 
 if __name__=="__main__":
     pcm = pcm('13_hush_hush.wav', 44)
