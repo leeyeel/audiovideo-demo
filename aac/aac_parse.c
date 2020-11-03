7cd2099b53dc62305c358d22ba9d685b282d6805
leo <mumuli52@gmail.com>
2020-11-04 00:21:29 +0800

    修改格式

--- aac/aac_parse.c
+++ aac/aac_parse.c
@@ -14,41 +14,30 @@ static unsigned char bufferPCM[BUFFER_PCM_MAX_LEN] = {0};
 int get_one_ADTS_frame(unsigned char* buffer, size_t buf_size, unsigned char* data ,size_t* data_size)
 {
     size_t size = 0;
- 
-    if(!buffer || !data || !data_size )
-    {
+    if(!buffer || !data || !data_size)
         return -1;
-    }
- 
-    while(1)
-    {
-        if(buf_size  < 7 )
-        {
-            return -1;
-        }
- 
+
+    while(buf_size >= 7){
         //帧同步，需要12个1,即0xFFF。若满足表示是个固定头信息
-		if ((buffer[0] == 0xff) && ((buffer[1] & 0xf0) == 0xf0))
-		{
-			size |= (((buffer[3] & 0x03)) << 11);//high 2 bit
-			size |= (buffer[4] << 3);//middle 8 bit
-			size |= ((buffer[5] & 0xe0) >> 5);//low 3bit
+        if ((buffer[0] == 0xff) && ((buffer[1] & 0xf0) == 0xf0)){
+            size |= (((buffer[3] & 0x03)) << 11);//high 2 bit
+            size |= (buffer[4] << 3);//middle 8 bit
+            size |= ((buffer[5] & 0xe0) >> 5);//low 3bit
             int channels = ((buffer[2] & 0x01) << 2 )| ( buffer[3] >> 6);
-			printf("size=%d, channels=%d\r\n", (int)size, channels);
-			break;
-		}
+            printf("size=%d, channels=%d\r\n", (int)size, channels);
+            break;
+        }
         --buf_size;
         ++buffer;
     }
- 
-    if(buf_size < size)
-    {
+
+    if(buf_size < size){
         return -1;
     }
- 
+
     memcpy(data, buffer, size);
     *data_size = size;
-    
+
     return 0;
 }
  
@@ -96,18 +85,15 @@ int main(int argc, char *argv[])
 
     //memset(bufferAAC,0, BUFFER_MAX_LEN);
     int bufsizeAAC = fread(bufferAAC, 1, BUFFER_MAX_LEN, infile);
-    while(get_one_ADTS_frame(bufferAAC, bufsizeAAC, frames, &size) == 0)
-    {
+    while(get_one_ADTS_frame(bufferAAC, bufsizeAAC, frames, &size) == 0){
         //decode ADTS frames
         pcm_data = (unsigned char*)NeAACDecDecode(hDecoder, &frame_info, frames, size);
 
-        if(frame_info.error > 0)
-        {
+        if(frame_info.error > 0){
             printf("[%d]%s\n",__LINE__, NeAACDecGetErrorMessage(frame_info.error));            
             return -1;
         }
-        else if(pcm_data && frame_info.samples > 0)
-        {
+        else if(pcm_data && frame_info.samples > 0){
             printf("frames info: bytesconsumed %lu, channels %u, header_type %u\
                     object_type %u, samples %lu, samplerate %lu\n", 
                     frame_info.bytesconsumed, 
@@ -116,17 +102,8 @@ int main(int argc, char *argv[])
                     frame_info.samplerate);
 
             size_t buf_sizePCM = frame_info.samples * frame_info.channels;
-            /*
-            //从双声道的数据中提取单通道  
-            for (int i = 0, j = 0; i<4096 && j<2048; i += 4, j += 2)
-            {
-            bufferPCM[j] = pcm_data[i];
-            bufferPCM[j + 1] = pcm_data[i + 1];
-            }
-            */
             memcpy(bufferPCM, pcm_data, buf_sizePCM);
             fwrite(pcm_data, 1, buf_sizePCM, outfile);
-
         }        
         bufsizeAAC -= size;
     }
